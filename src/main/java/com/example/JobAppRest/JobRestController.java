afaf5f898f7e40c9705d30a85da17eed2500b916
Chandraj N. Hejmady <chandrajhejmady@gmail.com>
2025-04-26 11:35:19 +0530

    Handle GET Request for all job posts

--- src/main/java/com/example/JobAppRest/JobRestController.java
+++ src/main/java/com/example/JobAppRest/JobRestController.java
@@ -1,4 +1,23 @@
 package com.example.JobAppRest;
 
+import com.example.JobAppRest.model.JobPost;
+import com.example.JobAppRest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@CrossOrigin(origins = "frontend_link") -> if connecting to frontend like react
 public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
 }
